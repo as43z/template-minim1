0c082aa36f8482a55b84289c235b5afd08fb228e
as43z <albertsigmaeng@gmail.com>
2021-04-22 18:39:26 +0200

    final template

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 // APP
 // start imports
-/**
- *  express
- *  morgan
- *  cors
- *  routes
- */
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
 // end imports
 
 // app
+const app = express();
 
 // middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
 // routes
+const apiPath = '/api';
 
 // global variables for app
+app.set('PORT', process.env.PORT || 8080);
+app.set('apiPath', apiPath);
 
 // export the app
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 // MAIN
 // start imports
-/**
- * app
- * startConnection
- */
+import { startConnection } from './database';
+import app from './app';
 // end imports
 
 async function main() {
     // base de datos:
+    startConnection();
 
     // PORT and BASE URI
-    const PORT = '';
-    const baseAPI = `http:localhost:${PORT}/`;
+    const PORT = app.get('PORT');
+    const apiPath = app.get('apiPath');
+    const baseAPI = `http:localhost:${PORT}${apiPath}`;
 
     // listen
-
+    app.listen(PORT);
 
     console.log(`[ SERVER STARTED ] server started @ ${baseAPI}`);
 }
